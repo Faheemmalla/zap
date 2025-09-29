2d0d9bd405cb3b5ddc9c08499ee3190e56a5798a
Faheemmalla <faheem.yousuf2004@gmail.com>
2025-09-29 09:15:54 +0530

    Initial Commit

---
+++ zap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zap
+//
+//  Created by faheem yousuf malla on 29/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zap/zapApp.swift
@@ -0,0 +1,17 @@
+//
+//  zapApp.swift
+//  zap
+//
+//  Created by faheem yousuf malla on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct zapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
